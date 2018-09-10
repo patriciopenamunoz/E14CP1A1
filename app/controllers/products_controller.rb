98019856281716ca4686a41d1271983dad7b5b2f
Patricio Andrés Peña Muñoz <patriciopenamunoz@gmail.com>
2018-09-10 20:15:08 -0300

    Allow create new products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,13 @@
 class ProductsController < ApplicationController
   def create
-    Product.create(product_params)
-    redirect_to Category.find(params['product']['category_id'])
+    @product = Category.find(params['category_id'])
+                       .products
+                       .build(product_params)
+    @product.save
+    redirect_to @product.category
   end
 
   def product_params
-    params.require('product').permit(%I[name price category_id])
+    params.require('product').permit(%I[name price])
   end
 end
