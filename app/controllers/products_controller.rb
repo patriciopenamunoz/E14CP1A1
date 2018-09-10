c389485e4dc01b916648959370fea4daf1233f39
Patricio Andrés Peña Muñoz <patriciopenamunoz@gmail.com>
2018-09-10 19:55:28 -0300

    Create new product form

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,10 @@
 class ProductsController < ApplicationController
+  def create
+    Product.create(product_params)
+    redirect_to Category.find(params['product']['category_id'])
+  end
+
+  def product_params
+    params.require('product').permit(%I[name price category_id])
+  end
 end
